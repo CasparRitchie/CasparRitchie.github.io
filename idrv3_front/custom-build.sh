b77437790ac96787e473f26b1fbaa4a4fef4033e
Caspar Ritchie <caspar.ritchie@lapiscine.pro>
2023-09-26 15:50:49 +0200

    Update build script paths

--- idrv3_front/custom-build.sh
+++ idrv3_front/custom-build.sh
@@ -9,8 +9,9 @@ cd build
 # Check if the build directory has the expected structure
 if [ ! -d "public" ]; then
   # Create a public directory inside build
-  mkdir public
+#   mkdir public
 
-  # Move all files from build to build/public except for the public directory itself
-  mv * public/ 2>/dev/null
+#   # Move all files from build to build/public except for the public directory itself
+#   mv * public/ 2>/dev/null
+cd ..
 fi
